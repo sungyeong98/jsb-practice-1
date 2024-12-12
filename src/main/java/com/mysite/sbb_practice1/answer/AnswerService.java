e7a05e85daf143379112a6ad4760f0d698db747a
sungyeong98 <am25227903@gmail.com>
2024-12-12 11:50:12 +0900

    feat: 답변 기능 구현

--- src/main/java/com/mysite/sbb_practice1/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb_practice1/answer/AnswerService.java
@@ -1,4 +1,24 @@
 package com.mysite.sbb_practice1.answer;
 
+import com.mysite.sbb_practice1.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
 public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+
 }
--- src/main/java/com/mysite/sbb_practice1/question/QuestionController.java
+++ src/main/java/com/mysite/sbb_practice1/question/QuestionController.java
@@ -5,23 +5,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
-    @GetMapping("/question/detail/{id}")
+    @GetMapping("/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = questionService.getQuestion(id);
         model.addAttribute("question", question);
