37b893b1ee90d1df33dba0d8c3dda9459cc146a5
sungyeong98 <am25227903@gmail.com>
2024-12-12 11:13:30 +0900

    feat: 질문목록 출력 코드 작성

--- src/main/java/com/mysite/sbb_practice1/question/QuestionController.java
+++ src/main/java/com/mysite/sbb_practice1/question/QuestionController.java
@@ -1,16 +1,23 @@
 package com.mysite.sbb_practice1.question;
 
+import org.springframework.ui.Model;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/question/list")
-    @ResponseBody
-    public String question() {
-        return "question";
+    public String list(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
     }
 
 }
