1f4694276f9dbed5f42ea88aa4f8ce39cb726ea2
sungyeong98 <am25227903@gmail.com>
2024-12-12 16:23:50 +0900

    feat: 페이징 기능 구현

--- src/main/java/com/mysite/sbb_practice1/question/QuestionController.java
+++ src/main/java/com/mysite/sbb_practice1/question/QuestionController.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb_practice1.question;
 
 import com.mysite.sbb_practice1.answer.AnswerForm;
+import org.springframework.data.domain.Page;
 import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -22,9 +23,9 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = questionService.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<Question> paging = questionService.getList(page);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
 
--- src/main/java/com/mysite/sbb_practice1/question/QuestionService.java
+++ src/main/java/com/mysite/sbb_practice1/question/QuestionService.java
@@ -2,7 +2,12 @@ package com.mysite.sbb_practice1.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import com.mysite.sbb_practice1.DataNotFoundException;
@@ -13,8 +18,11 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 
     public Question getQuestion(Integer id) {
