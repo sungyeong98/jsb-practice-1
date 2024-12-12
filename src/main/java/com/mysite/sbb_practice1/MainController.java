f9eb96146181842b4064a8dcf98f3befdb88fb8c
sungyeong98 <am25227903@gmail.com>
2024-12-12 10:13:17 +0900

    feat: sbb 기본 설정

---
+++ src/main/java/com/mysite/sbb_practice1/MainController.java
@@ -0,0 +1,16 @@
+package com.mysite.sbb_practice1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+
+}
---
+++ src/main/java/com/mysite/sbb_practice1/SbbPractice1Application.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_practice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbPractice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbPractice1Application.class, args);
+    }
+
+}
