53ccd66d01d48393964c14a75d68ad947605269a
sungyeong98 <am25227903@gmail.com>
2024-12-13 11:39:16 +0900

    feat: 검색 기능 구현

--- src/main/java/com/mysite/sbb_practice1/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb_practice1/question/QuestionRepository.java
@@ -2,8 +2,10 @@ package com.mysite.sbb_practice1.question;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Page<Question> findAll(Pageable pageable);
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
